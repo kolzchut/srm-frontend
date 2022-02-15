70781ca7211ad2cd3ffeccea58e8a08326d193b9
Adam Kariv <adam.kariv@gmail.com>
2022-02-15 16:54:22 +0200

    Move to another GA property to test UA

--- projects/srm/src/environments/environment.prod.ts
+++ projects/srm/src/environments/environment.prod.ts
@@ -19,7 +19,8 @@ export const environment = {
   taxonomyResponsesURL: '/responses.json',
   suggestChangesForm: 'https://airtable.com/shriuBukhE2B2Loy8',
   mapStyle: 'mapbox://styles/srm-kolzchut/cksprr4sy0hbg18o5ct2ty2oc',
-  gaTag: 'G-7ZHM371DDF',
+  gaTag: 'G-0FXK63SSNX',
+  // gaTag: 'G-7ZHM371DDF',
   sitemapUrl: 'https://srm.datacity.org.il/dataset/6fbf9571-b66e-4264-a4e6-605539517b22/resource/84a08676-b695-429f-a487-f9084b0b4888/download/sitemap.xml',
 };
 
