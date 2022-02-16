791406a184c1dbd213859969487298cec895f828
Adam Kariv <adam.kariv@gmail.com>
2022-02-16 11:54:33 +0200

    kolzchut/srm#105 break title to two lines in search results as well

--- projects/srm/src/app/highlighter.service.ts
+++ projects/srm/src/app/highlighter.service.ts
@@ -11,16 +11,16 @@ export class HighlighterService {
 
   highlight(text: string) {
     const query = this.search.searchQuery;
-    text = text.split(' ').join('&nbsp;');
+    // text = text.split(' ').join('&nbsp;');
     if (query) {
       const parts = query.split(/\s+/);
       for (const part of parts) {
         if (part && part.length > 0) {
           const regex = new RegExp(part, 'gi');
-          text = text.replace(regex, match => `</span><strong>${match}</strong><span>`);  
+          text = text.replace(regex, match => `<strong>${match}</strong>`);
         }
       }
-      text = `<span>${text}</span>`.split('<span></span>').join('');
+      // text = `<span>${text}</span>`.split('<span></span>').join('');
     }
     return text;
   }
