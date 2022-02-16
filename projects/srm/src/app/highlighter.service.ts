98596d2b7a0c307b611d0bbe23c69f3785ed5833
Adam Kariv <adam.kariv@gmail.com>
2022-02-16 16:20:46 +0200

    kolzchut/srm#105 fix style for place search

--- projects/srm/src/app/highlighter.service.ts
+++ projects/srm/src/app/highlighter.service.ts
@@ -16,8 +16,12 @@ export class HighlighterService {
       const parts = query.split(/\s+/);
       for (const part of parts) {
         if (part && part.length > 0) {
-          const regex = new RegExp(part, 'gi');
-          text = text.replace(regex, match => `<strong>${match}</strong>`);
+          const regex = new RegExp(part + '[^ ]', 'gi');
+          text = text.replace(regex, match => `<strong>${match.slice(0, -1)}</strong>${match.slice(-1)}`);
+          const regex_end = new RegExp(part + '$', 'gi');
+          text = text.replace(regex_end, match => `<strong>${match}</strong>`);
+          const regex_sp = new RegExp(part + ' ', 'gi');
+          text = text.replace(regex_sp, match => `<strong>${match.slice(0, -1)}</strong>&nbsp;`);
         }
       }
       // text = `<span>${text}</span>`.split('<span></span>').join('');
