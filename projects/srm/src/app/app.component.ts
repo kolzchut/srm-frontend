5cbf3f9703a4d1f2108b7a1cfa9db6dec323173b
Adam Kariv <adam.kariv@gmail.com>
2022-12-28 13:05:18 +0200

    Space page views

--- projects/srm/src/app/app.component.ts
+++ projects/srm/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { SeoSocialShareService } from 'ngx-seo';
+import { debounceTime, filter } from 'rxjs';
 import { environment } from '../environments/environment';
 import { AnalyticsService } from './analytics.service';
 import { PlatformService } from './platform.service';
@@ -18,15 +19,17 @@ export class AppComponent {
       private platform: PlatformService, 
       private seo: SeoSocialShareService) {
     if (environment.gaTag) {
-      this.router.events.subscribe(event => {
-        if(event instanceof NavigationEnd) {
-          platform.browser(() => {
-            console.log('PAGE VIEW', event.urlAfterRedirects);
-            window.gtag && gtag('config', environment.gaTag, {
-              'page_path': event.urlAfterRedirects
-            });  
-          })
-        }
+      this.router.events.pipe(
+        filter((event) => event instanceof NavigationEnd),
+        debounceTime(250)
+      ).subscribe((event_) => {
+        platform.browser(() => {
+          const event = event_ as NavigationEnd;
+          console.log('PAGE VIEW', event.urlAfterRedirects);
+          window.gtag && gtag('config', environment.gaTag, {
+            'page_path': event.urlAfterRedirects
+          });  
+        })
       });
     }
     this.seo.setData({
