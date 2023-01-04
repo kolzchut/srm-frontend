3880f6920e1b10f56d76ff0d25a31bdf57ed5949
Adam Kariv <adam.kariv@gmail.com>
2023-01-04 22:35:37 +0200

    initialNavigation -> enabledBlocking

--- projects/srm/src/app/app-routing.module.ts
+++ projects/srm/src/app/app-routing.module.ts
@@ -51,7 +51,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled',
+    initialNavigation: 'enabledBlocking',
     // enableTracing: true
 })],
   exports: [RouterModule]
