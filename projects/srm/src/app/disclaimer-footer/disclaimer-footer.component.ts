2e53092354b8fb305c81887eaea9d36f76cb6fdd
Adam Kariv <adam.kariv@gmail.com>
2023-01-08 16:23:02 +0200

    Missing file

---
+++ projects/srm/src/app/disclaimer-footer/disclaimer-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disclaimer-footer',
+  templateUrl: './disclaimer-footer.component.html',
+  styleUrls: ['./disclaimer-footer.component.less']
+})
+export class DisclaimerFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
