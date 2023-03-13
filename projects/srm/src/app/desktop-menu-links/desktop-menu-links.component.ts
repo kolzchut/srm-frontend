c6b592512da16d6508ebb640bafdb7eddef747b8
Adam Kariv <adam.kariv@gmail.com>
2023-03-13 12:09:54 +0200

    More accessibility, wip kolzchut/srm#445

--- projects/srm/src/app/desktop-menu-links/desktop-menu-links.component.ts
+++ projects/srm/src/app/desktop-menu-links/desktop-menu-links.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-desktop-menu-links',
   templateUrl: './desktop-menu-links.component.html',
   styleUrls: ['./desktop-menu-links.component.less'],
   host: {
-    'class': 'layout-desktop'
+    '[class.layout-desktop]': '!footer',
+    '[class.footer]': 'footer',
+    '[class.homepage]': 'homepage',
   }
 })
 export class DesktopMenuLinksComponent implements OnInit {
+
+  @Input() footer = false;
+  @Input() homepage = false;
 
   constructor() { }
 
