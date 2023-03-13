2c812ff1acb4781af58950245c2ad07e365ac52d
Adam Kariv <adam.kariv@gmail.com>
2023-03-13 13:16:07 +0200

    More accessibility, wip kolzchut/srm#445

--- projects/srm/src/app/disclaimer-footer/disclaimer-footer.component.ts
+++ projects/srm/src/app/disclaimer-footer/disclaimer-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-disclaimer-footer',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./disclaimer-footer.component.less']
 })
 export class DisclaimerFooterComponent implements OnInit {
+
+  @Input() homepage = false;
 
   constructor() { }
 
