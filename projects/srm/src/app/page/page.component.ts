4f5323f5febaaaf758a5c0c42d74267627cedfd8
Adam Kariv <adam.kariv@gmail.com>
2023-03-20 15:46:02 +0200

    Fix for response count and filter count

--- projects/srm/src/app/page/page.component.ts
+++ projects/srm/src/app/page/page.component.ts
@@ -285,11 +285,12 @@ export class PageComponent implements OnInit {
       if (params.original_query && window.gtag) {
         const title = params.original_query;
         console.log('EVENT search', title);
+        const responseCount = (params.filter_responses || []).length;
         window.gtag({
           event: 'srm:search',
           search_term: title,
-          filter_count: params.allTaxonomyIds.length,
-          filter_responses_count: (params.filter_responses || []).length,
+          filter_count: params.allTaxonomyIds.filter(x => !!x).length - responseCount,
+          filter_responses_count: responseCount,
           filter_response_categories_count: (params.filter_response_categories || []).length,
           filter_national: params.national ? 1 : 0,
           landing_page: this.isLandingPage ? 1 : 0,
