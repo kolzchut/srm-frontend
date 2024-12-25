ab467a03c3ae710fae1daaa649d936d20e840ac6
Amir <amirb4@gmail.com>
2024-12-25 17:35:40 +0200

    filters with 0

--- projects/srm/src/app/search-filters-bar/search-filters-bar.component.ts
+++ projects/srm/src/app/search-filters-bar/search-filters-bar.component.ts
@@ -21,4 +21,11 @@ export class SearchFiltersBarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  isShowFilter(item: any): boolean {
+    if (!item) return false;
+    const isSituationSelected = this.filtersState.allFilteredSituations.includes(item.key);
+    const isResponseSelected = this.filtersState.allFilteredResponses.includes(item.key);
+    return isSituationSelected || isResponseSelected || item.doc_count > 0;
+  }
 }
