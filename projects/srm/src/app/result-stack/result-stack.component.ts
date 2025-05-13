406e9d5a835b070d28591f53433a348bf08b8e3e
jonat <jonathan@amitkohn.com>
2025-05-13 14:44:05 +0300

    order branch list by city

--- projects/srm/src/app/result-stack/result-stack.component.ts
+++ projects/srm/src/app/result-stack/result-stack.component.ts
@@ -39,7 +39,8 @@ export class ResultStackComponent implements OnInit {
         (h as any)['__city_count'] = h.national_service ? 9999 : (cityNames[cityName] || 0);
       });
       // Sort collapse_hits by __city_count ascending
-      this.result.collapse_hits.sort((a, b) => a.branch_city.localeCompare(b.branch_city, 'he-IL'));
+      this.result.collapse_hits = this.result.collapse_hits
+        .sort((a, b) => a.branch_city.localeCompare(b.branch_city, 'he-IL'));
       this.result.collapse_hits = this.result.collapse_hits.filter((h) => h.card_id !== this.result.card_id);
     }
     if (this.showCount === -1 && this.collapsibleCount > 0) {
