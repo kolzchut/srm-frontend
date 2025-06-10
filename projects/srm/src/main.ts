7b05e4f1f1cfc0057db50ecc406de8c3e75b00de
arielrepos <ariel_ohana@webiks.com>
2025-06-10 14:56:53 +0300

    URL router

--- projects/srm/src/main.ts
+++ projects/srm/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import * as Sentry from "@sentry/angular-ivy";
+import {urlRouter} from "./services/urlRouter";
+
+
+urlRouter();
 
 if (environment.production) {
   Sentry.init({
