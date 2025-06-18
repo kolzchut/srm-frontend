7806ae699fdb8c449f0f3672639fdb1739fc2309
arielrepos <ariel_ohana@webiks.com>
2025-06-18 12:06:26 +0300

    Update budget limits in angular.json for initial and component styles

--- projects/srm/src/services/urlRouter.ts
+++ projects/srm/src/services/urlRouter.ts
@@ -1,40 +1,32 @@
 // @ts-ignore
 import inactiveCardIdToServiceName from '../configurations/inactiveCardIdToServiceName.json';
 
-const getEndOfCardId = (urlPart: string) =>{
+const getEndOfCardId = (urlPart: string) => {
   const hashIndex = urlPart.indexOf('#');
   const questionIndex = urlPart.indexOf('?');
 
   let indexOfEndCardIdValue: number;
+  if (hashIndex === -1) indexOfEndCardIdValue = questionIndex;
+  else if (questionIndex === -1) indexOfEndCardIdValue = hashIndex;
+  else indexOfEndCardIdValue = Math.min(hashIndex, questionIndex);
 
-  if (hashIndex === -1) {
-    indexOfEndCardIdValue = questionIndex;
-  } else if (questionIndex === -1) {
-    indexOfEndCardIdValue = hashIndex;
-  } else {
-    indexOfEndCardIdValue = Math.min(hashIndex, questionIndex);
-  }
   return indexOfEndCardIdValue;
 }
 
 
 const checkInactiveCardId = () => {
   const urlParts = window.location.href.split('/');
-  console.log("Ariel - urlParts", urlParts);
   const cardIdIndex = urlParts.findIndex(part => part === 'c') + 1;
-  console.log("Ariel - cardIdIndex", cardIdIndex);
-  if (cardIdIndex === 0) return;
+  if (cardIdIndex === 0) return false;
   const rawCardIdValue: string = urlParts[cardIdIndex];
   const indexOfEndCardIdValue = getEndOfCardId(rawCardIdValue)
-  console.log("Ariel - indexOfEndCardIdValue", indexOfEndCardIdValue);
-  if (indexOfEndCardIdValue === -1) return;
+  if (indexOfEndCardIdValue === -1) return false;
   const cardIdValue = rawCardIdValue.slice(0, indexOfEndCardIdValue);
-  if (!inactiveCardIdToServiceName[cardIdValue]) return;
+  if (!inactiveCardIdToServiceName[cardIdValue]) return false;
   const serviceName = inactiveCardIdToServiceName[cardIdValue];
-  console.log("Ariel - serviceName", serviceName);
   const newUrl = urlParts.slice(0, cardIdIndex - 2).join('/') + '/' + serviceName;
-  console.log("Ariel - newUrl", newUrl);
   window.location.replace(newUrl);
+  return true;
 }
 
 const checkOldUrl = () => {
@@ -42,15 +34,25 @@ const checkOldUrl = () => {
   let newUrl = undefined;
 
   const isOldURL = urlParts[7]?.length === 1;
-  if (isOldURL) {
-    newUrl = urlParts.slice(0, 7).join('/');
-  }
-  if (!newUrl) return;
+  if (isOldURL) newUrl = urlParts.slice(0, 7).join('/');
+  if (!newUrl) return false;
   window.location.replace(newUrl);
+  return true;
+}
+const notFoundToHomePage = () => {
+  const urlParts = window.location.href.split('/');
+  const isNotFound = urlParts[urlParts.length - 1] === 'not-found';
+  if (!isNotFound) return false;
+  const newUrl = urlParts.slice(0, -1).join('/');
+  window.location.replace(newUrl);
+  return true;
+
 }
 
 export const urlRouter = () => {
-  checkOldUrl();
-  checkInactiveCardId();
+  if(checkOldUrl()) return;
+  if(checkInactiveCardId()) return;
+
+  notFoundToHomePage();
 };
 
